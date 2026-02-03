dd4eeb6eb877651772319668cf185711fcb9b748
hend <heniandong@a-eye.cn>
2026-02-03 18:11:36 +0800

    1

--- mifss-apps/mifss-app-ipt/src/main/java/cn/hsa/hsaf/auth/security/filter/ApiAuthorizationFilter.java
+++ mifss-apps/mifss-app-ipt/src/main/java/cn/hsa/hsaf/auth/security/filter/ApiAuthorizationFilter.java
@@ -3,7 +3,7 @@ package cn.hsa.hsaf.auth.security.filter;
 import cn.hsa.hsaf.auth.security.utils.ApiVerifyUtil;
 import cn.hsa.hsaf.core.framework.web.WrapperResponse;
 import cn.hsa.ims.common.utils.AeyeSpringContextUtils;
-import com.alibaba.fastjson.JSONObject;
+import cn.hutool.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -29,16 +29,15 @@ public class ApiAuthorizationFilter implements Filter {
         }
 
         if (isPrivilege == null || !isPrivilege) {
-            oldDoFilter(request, response, chain);
+            doHsaFilter(request, response, chain);
         } else {
             chain.doFilter(request, response);
         }
     }
 
-    private void oldDoFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest httprequest = (HttpServletRequest) request;
-        HttpServletResponse httpresponse = (HttpServletResponse) response;
+    public void doHsaFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httprequest = (HttpServletRequest)request;
+        HttpServletResponse httpresponse = (HttpServletResponse)response;
         ApiVerifyUtil apiVerifyUtil = new ApiVerifyUtil();
 
         try {
@@ -54,34 +53,32 @@ public class ApiAuthorizationFilter implements Filter {
                 JSONObject res = new JSONObject();
                 res.put("code", 110001);
                 res.put("type", WrapperResponse.ResponseType.TYPE_ERROR);
-                res.put("message", "API没有访问权限!!!");
+                res.put("message", "API没有访问权限!");
                 res.put("data", "");
                 out.append(res.toString());
                 out.flush();
                 out.close();
             }
-        } catch (Exception var10) {
-            log.error(var10.toString(), var10);
+        } catch (Exception ex) {
+            log.error(ex.toString(), ex);
             httpresponse.setCharacterEncoding("UTF-8");
             httpresponse.setContentType("application/json; charset=utf-8");
             PrintWriter out = httpresponse.getWriter();
             JSONObject res = new JSONObject();
             res.put("code", 110001);
             res.put("type", WrapperResponse.ResponseType.TYPE_ERROR);
-            res.put("message", "API没有访问权限!!!");
+            res.put("message", "API没有访问权限!");
             res.put("data", "");
             out.append(res.toString());
             out.flush();
             out.close();
         }
+
     }
 
-    @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
-    @Override
     public void destroy() {
 
     }
