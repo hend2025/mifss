515765b0650ee51fdf01c660ebd4fb43bc014fb7
hend <835701898@qq.com>
2026-02-04 22:46:52 +0800

    1

--- mifss-mainline-biz/mifss-biz-bio/src/main/java/com/aeye/mifss/bio/bo/FaceFturBo.java
+++
@@ -1,7 +0,0 @@
-package com.aeye.mifss.bio.bo;
-
-import com.aeye.mifss.bio.entity.FaceFturDO;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface FaceFturBo extends IService<FaceFturDO> {
-}
