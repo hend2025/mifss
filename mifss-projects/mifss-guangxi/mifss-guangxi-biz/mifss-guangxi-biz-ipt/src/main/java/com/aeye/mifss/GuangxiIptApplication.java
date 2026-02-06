e589fe9bbcfffcd5af72803c2c4609a19151439f
hend <835701898@qq.com>
2026-02-06 11:53:18 +0800

    1

--- mifss-projects/mifss-guangxi/mifss-guangxi-biz/mifss-guangxi-biz-ipt/src/main/java/com/aeye/mifss/GuangxiIptApplication.java
+++ mifss-projects/mifss-guangxi/mifss-guangxi-biz/mifss-guangxi-biz-ipt/src/main/java/com/aeye/mifss/GuangxiIptApplication.java
@@ -1,11 +1,15 @@
 package com.aeye.mifss;
 
+import cn.hsa.ims.common.config.WildcardControllerFilter;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(scanBasePackages = { "com.aeye.mifss" })
+@SpringBootApplication
 @EnableDubbo
+@ComponentScan(basePackages = {"com.aeye.mifss"}, excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM, classes = WildcardControllerFilter.class ))
 public class GuangxiIptApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuangxiIptApplication.class, args);
--- mifss-projects/mifss-guangxi/mifss-guangxi-biz/mifss-guangxi-biz-ipt/src/main/java/com/aeye/mifss/ipt/controller/GuangxiIptFaceController.java
+++ mifss-projects/mifss-guangxi/mifss-guangxi-biz/mifss-guangxi-biz-ipt/src/main/java/com/aeye/mifss/ipt/controller/GuangxiIptFaceController.java
@@ -3,19 +3,30 @@ package com.aeye.mifss.ipt.controller;
 import com.aeye.mifss.bio.dto.FaceImageReq;
 import com.aeye.mifss.bio.service.RpcFaceRecognitionService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/face")
 public class GuangxiIptFaceController extends IptFaceController {
 
-    @DubboReference(check = false, group = "${mifss.dubbo.group:}")
-    protected RpcFaceRecognitionService faceRecognitionService;
+    @DubboReference(check = false, group = "${mifss.dubbo.group:guangxi}")
+    private RpcFaceRecognitionService faceRecognitionService;
 
-    @Override
-    public String detectFace(@RequestBody FaceImageReq request) {
-        System.out.println(" Guangxi  IptFaceController ");
+    @PostMapping("/authenticate")
+    public boolean authenticate(@RequestBody FaceImageReq request) {
+        System.out.println(" Guangxi   authenticate ");
         if (request == null || request.getImageData() == null || request.getImageData().isEmpty()) {
-            return "Error: Empty Image Data";
+            return false;
         }
+        return faceRecognitionService.authenticate(request);
+    }
+
+    @PostMapping("/detectFace22")
+    public String detectFace22(@RequestBody FaceImageReq request) {
+        System.out.println(" Guangxi   detectFace22 ");
         return faceRecognitionService.detectFace(request);
     }
 
