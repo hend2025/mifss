fe61e77b2fed346d40a8c65aa2d7ab20586ab9c1
hend <835701898@qq.com>
2026-02-08 22:33:37 +0800

    ---  现场版本rpc ok  --------

--- mifss-projects/mifss-guangxi/mifss-guangxi-biz/mifss-guangxi-biz-ipt/src/main/java/com/aeye/mifss/ipt/controller/GuangxiFaceController.java
+++ mifss-projects/mifss-guangxi/mifss-guangxi-biz/mifss-guangxi-biz-ipt/src/main/java/com/aeye/mifss/ipt/controller/GuangxiFaceController.java
@@ -5,18 +5,21 @@ import com.aeye.mifss.bio.dto.FaceImageReq;
 import com.aeye.mifss.bio.service.RpcFaceRecognitionService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "RPC测试人脸接口")
 @RestController
 @RequestMapping("/face")
-public class TestController {
+public class GuangxiFaceController extends FaceController{
 
     @Autowired(required = false)
     private RpcFaceRecognitionService rpcFaceRecognitionService;
 
-    @PostMapping("/detect")
-    public WrapperResponse<String> faceDetect(@RequestBody FaceImageReq request) {
+    @PostMapping("/faceDetectNew")
+    public WrapperResponse<String> faceDetectNew(@RequestBody FaceImageReq request) {
         if (request == null || request.getImageData() == null || request.getImageData().isEmpty()) {
             return WrapperResponse.fail("Error: Empty Image Data");
         }
