6c9cac0cfae70de95d4e2f596c0c700de00c3f79
hend <835701898@qq.com>
2026-02-23 09:46:38 +0800

    1

--- mifss-mainline/mifss-biz/mifss-biz-bio/src/main/java/com/aeye/mifss/bio/controller/TestMqProducerController.java
+++ mifss-mainline/mifss-biz/mifss-biz-bio/src/main/java/com/aeye/mifss/bio/controller/TestMqProducerController.java
@@ -2,8 +2,6 @@ package com.aeye.mifss.bio.controller;
 
 import cn.hsa.hsaf.core.mq.MQMessage;
 import cn.hsa.hsaf.core.mq.MQProducer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mq")
 public class TestMqProducerController {
-    private static final Logger log = LoggerFactory.getLogger(TestMqProducerController.class);
 
     @Autowired(required = false)
     private MQProducer mqProducer;
@@ -34,14 +31,13 @@ public class TestMqProducerController {
                 System.out.println(stats);
 
                 System.out.println("消息发送成功");
-                log.info("消息发送成功: topic={}, tag={}", "MQ_IMS_CRTF_TEST", "crtf");
                 return "消息发送成功";
             } else {
                 return "未找到MQ生产者或配置";
             }
         } catch (Exception e) {
-            log.error("消息发送异常：" + e.getMessage(), e);
             return "消息发送异常";
         }
     }
+
 }
